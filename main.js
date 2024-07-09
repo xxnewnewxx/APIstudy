570b2802b5d76e2fbc4f57c5d362b8fc9bb8459b
younghwa <xxnewnewxx0127@gmail.com>
2024-07-10 07:42:18 +0900

    two try

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
 let news = [];
 const getLatesNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
+    `https://rad-sawine-85f04e.netlify.app/top-headlines?q=${keyword}&country=kr&pagesize=${PAGE_SIZE}`
   );
   //   console.log("uuu", url);
   const response = await fetch(url);
