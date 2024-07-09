4d5b833fffa001ea639023ea7af6ed04dfe5f8f4
younghwa <xxnewnewxx0127@gmail.com>
2024-07-10 08:57:56 +0900

    ten try

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_KEY = ``;
+const API_KEY = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
 const pageSize = 10;
 let page = 2;
 let category = "";
@@ -6,7 +6,7 @@ let keyword = "";
 let news = [];
 const getLatesNews = async () => {
   const url = new URL(
-    `https://rad-sawine-85f04e.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
+    `https://rad-sawine-85f04e.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}&category=${category}&keyword=${keyword}`
   );
   //   console.log("uuu", url);
   const response = await fetch(url);
