0727b4484fd9a1a65c29da8115743a8418ac532c
younghwa <xxnewnewxx0127@gmail.com>
2024-07-10 08:03:13 +0900

    six try

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
-
+const pageSize = 10;
+let page = 2;
+let category = "";
+let keyword = "";
 let news = [];
 const getLatesNews = async () => {
-  const url = new URL(`https://rad-sawine-85f04e.netlify.app/top-headlines`);
+  const url = new URL(
+    `https://rad-sawine-85f04e.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
+  );
   //   console.log("uuu", url);
   const response = await fetch(url);
   const data = await response.json();
