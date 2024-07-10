cbc25879b57a3b4f49b82384616b4decd8bdc2be
younghwa <xxnewnewxx0127@gmail.com>
2024-07-10 09:19:48 +0900

    eleven try

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let keyword = "";
 let news = [];
 const getLatesNews = async () => {
   const url = new URL(
-    `https://rad-sawine-85f04e.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}&category=${category}&keyword=${keyword}`
+    `https://rad-sawine-85f04e.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}&category=${category}&keyword=${keyword}&apiKey=${API_KEY}`
   );
   //   console.log("uuu", url);
   const response = await fetch(url);
