5b39e865492247f7eb691dff3d005fb09d8d3abb
younghwa <xxnewnewxx0127@gmail.com>
2024-07-11 03:01:01 +0900

    news API study

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
-const API_KEY = `https://noona-times-v2.netlify.app/top-headlines`;
-const pageSize = 10;
-let page = 2;
-let category = "";
-let keyword = "";
-let news = [];
+const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
+
+let newsList = [];
 const getLatesNews = async () => {
-  const url = new URL(`https://rad-sawine-85f04e.netlify.app/top-headlines`);
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
+  );
   //   console.log("uuu", url);
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("santa", news);
+  newsList = data.articles;
+  render();
+  console.log("ddddddd", newsList);
 };
 
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src=${news.urlToImage} />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.tltie}</h2>
+            <p>
+           ${news.description}
+            </p>
+            <div>   ${news.source.name}   * ${news.publishedAt}</div>
+          </div>
+        </div>`
+    )
+    .join("");
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 getLatesNews();
